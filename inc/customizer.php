da831c54d7b4cd046245aab34ec781657f8c7e55
unknown <raymond.gothaii@gmail.com>
2016-01-01 13:29:52 -0500

    added social icons to customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,6 +10,14 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
+function my_customizer_social_media_array() {
+ 
+	/* store social site names in array */
+	$social_sites = array('twitter', 'facebook', 'google-plus', 'flickr', 'pinterest', 'youtube', 'tumblr', 'dribbble', 'rss', 'linkedin', 'instagram', 'email');
+ 
+	return $social_sites;
+}
+
 function rgdeuce_theme_customizer( $wp_customize ) {
 	
     // Logo upload
@@ -164,6 +172,35 @@ $wp_customize->add_setting( 'rgdeuce_header_color', array(
 		'settings'              => 'rgdeuce_theme_options[rgdeuce_inline_css]',
 		'type'                  => 'textarea'
 	) );
+//Social Icons
+	
+ 
+	$wp_customize->add_section( 'my_social_settings', array(
+			'title'    => __('Social Media Icons', 'text-domain'),
+			'priority' => 35,
+	) );
+ 
+	$social_sites = my_customizer_social_media_array();
+	$priority = 5;
+ 
+	foreach($social_sites as $social_site) {
+ 
+		$wp_customize->add_setting( "$social_site", array(
+				'type'              => 'theme_mod',
+				'capability'        => 'edit_theme_options',
+				'sanitize_callback' => 'esc_url_raw'
+		) );
+ 
+		$wp_customize->add_control( $social_site, array(
+				'label'    => __( "$social_site url:", 'text-domain' ),
+				'section'  => 'my_social_settings',
+				'type'     => 'text',
+				'priority' => $priority,
+		) );
+ 
+		$priority = $priority + 5;
+	}
+
 }
 add_action( 'customize_register', 'rgdeuce_theme_customizer' );
 
@@ -306,3 +343,45 @@ function rgdeuce_add_customizer_css() {
 
 add_action( 'wp_head', 'rgdeuce_add_customizer_css' );
 
+
+/* takes user input from the customizer and outputs linked social media icons */
+function my_social_media_icons() {
+ 
+    $social_sites = my_customizer_social_media_array();
+ 
+    /* any inputs that aren't empty are stored in $active_sites array */
+    foreach($social_sites as $social_site) {
+        if( strlen( get_theme_mod( $social_site ) ) > 0 ) {
+            $active_sites[] = $social_site;
+        }
+    }
+ 
+    /* for each active social site, add it as a list item */
+        if ( ! empty( $active_sites ) ) {
+ 
+            echo "<ul class='social-media-icons'>";
+ 
+            foreach ( $active_sites as $active_site ) {
+ 
+	            /* setup the class */
+		        $class = 'fa fa-' . $active_site;
+ 
+                if ( $active_site == 'email' ) {
+                    ?>
+                    <li>
+                        <a class="email" target="_blank" href="mailto:<?php echo antispambot( is_email( get_theme_mod( $active_site ) ) ); ?>">
+                            <i class="fa fa-envelope" title="<?php _e('email icon', 'text-domain'); ?>"></i>
+                        </a>
+                    </li>
+                <?php } else { ?>
+                    <li>
+                        <a class="<?php echo $active_site; ?>" target="_blank" href="<?php echo esc_url( get_theme_mod( $active_site) ); ?>">
+                            <i class="<?php echo esc_attr( $class ); ?>" title="<?php printf( __('%s icon', 'text-domain'), $active_site ); ?>"></i>
+                        </a>
+                    </li>
+                <?php
+                }
+            }
+            echo "</ul>";
+        }
+}
